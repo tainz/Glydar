9ed8f3846022dea1fea36633fd1bee8d290922bc
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-21 06:34:53 -0700

    Added stealth and arrow.

---
+++ src/main/java/net/cwserver/netty/packet/client/Packet8Stealth.java
@@ -0,0 +1,14 @@
+package net.cwserver.netty.packet.client;
+
+import io.netty.buffer.ByteBuf;
+import net.cwserver.netty.packet.CubeWorldPacket;
+
+@CubeWorldPacket.Packet(id = 8)
+public class Packet8Stealth extends CubeWorldPacket {
+
+    byte[] unknowndata;
+    @Override
+    public void decode(ByteBuf buf) {
+        buf.readBytes(unknowndata, 0, 40);
+    }
+}

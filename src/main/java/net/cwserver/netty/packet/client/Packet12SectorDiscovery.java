42b2cdecb6edc9ebd47695bb13bb4a8fa643b7de
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-21 06:44:29 -0700

    Sector Discovery

---
+++ src/main/java/net/cwserver/netty/packet/client/Packet12SectorDiscovery.java
@@ -0,0 +1,16 @@
+package net.cwserver.netty.packet.client;
+
+import io.netty.buffer.ByteBuf;
+import net.cwserver.netty.packet.CubeWorldPacket;
+
+@CubeWorldPacket.Packet(id = 12)
+public class Packet12SectorDiscovery extends CubeWorldPacket {
+    int sectorX;
+    int sectorZ;
+
+    @Override
+    public void decode(ByteBuf buf) {
+        sectorX = buf.readInt();
+        sectorZ = buf.readInt();
+    }
+}

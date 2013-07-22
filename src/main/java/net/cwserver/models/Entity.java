9e52ef30d12009ee0aa0e287c94cd3bf420ddfb0
Glydar <admin@glydar.org>
2013-07-22 13:08:19 +0200

    Entity .hC + .e

--- src/main/java/net/cwserver/models/Entity.java
+++ src/main/java/net/cwserver/models/Entity.java
@@ -15,4 +15,21 @@ public class Entity {
 	public static long getNewEntityID() {
 		return ENTITY_ID++;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Entity entity = (Entity) o;
+
+		if (entityID != entity.entityID) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (entityID ^ (entityID >>> 32));
+	}
 }

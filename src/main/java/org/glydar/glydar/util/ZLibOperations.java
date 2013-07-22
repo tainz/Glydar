dfad03a54a1a12557741a5606bea0e3c19a748ef
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-22 10:47:37 -0700

    ZLib decompression

---
+++ src/main/java/org/glydar/glydar/util/ZLibOperations.java
@@ -0,0 +1,30 @@
+package org.glydar.glydar.util;
+
+import java.io.ByteArrayOutputStream;
+import java.util.zip.Inflater;
+
+public class ZLibOperations {
+    public static byte[] decompress(byte[] data) throws Exception{
+        if(data.length == 0)
+            return null;
+
+        Inflater inf = new Inflater();
+        inf.setInput(data);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(data.length);
+
+        byte[] buf = new byte[1024];
+
+        while (!inf.finished()) {
+
+            int decompressed = inf.inflate(buf);
+
+            baos.write(buf, 0, decompressed);
+        }
+
+        inf.end();
+        baos.close();
+
+        return baos.toByteArray();
+    }
+}

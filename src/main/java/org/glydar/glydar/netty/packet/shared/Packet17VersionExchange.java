48390519f57ddc8b8e88bc487b6139a2dee9ce25
Glydar <admin@glydar.org>
2013-07-23 00:12:11 +0200

    Merged P17 s+c

---
+++ src/main/java/org/glydar/glydar/netty/packet/shared/Packet17VersionExchange.java
@@ -0,0 +1,40 @@
+package org.glydar.glydar.netty.packet.shared;
+
+import io.netty.buffer.ByteBuf;
+import org.glydar.glydar.models.Player;
+import org.glydar.glydar.models.ServerEntity;
+import org.glydar.glydar.netty.packet.CubeWorldPacket;
+import org.glydar.glydar.netty.packet.server.Packet15Seed;
+import org.glydar.glydar.netty.packet.server.Packet16Join;
+
+@CubeWorldPacket.Packet(id = 17)
+public class Packet17VersionExchange extends CubeWorldPacket {
+	int version;
+
+	public Packet17VersionExchange() {
+
+	}
+
+	public Packet17VersionExchange(int serverVersion) {
+		this.version = serverVersion;
+	}
+
+	@Override
+	protected void internalDecode(ByteBuf buf) {
+		version = buf.readInt();
+	}
+
+	@Override
+	public void receivedFrom(Player ply) {
+		System.out.println("Client from " + ply.getChannelContext().channel().remoteAddress() + " says hi! Version " + version + " ent id "+ply.entityID);
+
+		ply.sendPacket(new Packet16Join(ply.entityID));
+		ply.sendPacket(new Packet15Seed(69));
+		ply.sendPacket(new Packet10Chat("Server powered by Glydar 0.0.1-SNAPSHOT", ServerEntity.INSTANCE));
+	}
+
+    @Override
+    protected void internalEncode(ByteBuf buf) {
+        buf.writeInt(version);
+    }
+}

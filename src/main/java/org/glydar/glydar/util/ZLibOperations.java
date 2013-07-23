49dd2b068d948430785869a237aa63307184f5c3
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-07-23 09:11:12 -0700

    ZLib Compression Support

--- src/main/java/org/glydar/glydar/util/ZLibOperations.java
+++ src/main/java/org/glydar/glydar/util/ZLibOperations.java
@@ -1,9 +1,33 @@
 package org.glydar.glydar.util;
 
 import java.io.ByteArrayOutputStream;
+import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 public class ZLibOperations {
+    public static byte[] compress(byte[] data) throws Exception {
+        Deflater def = new Deflater();
+        def.setInput(data);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(data.length);
+
+        def.finish();
+
+        byte[] buf = new byte[1024];
+
+        while (!def.finished()) {
+
+            int compressed = def.deflate(buf);
+
+            baos.write(buf, 0, compressed);
+        }
+
+        baos.close();
+
+        return baos.toByteArray();
+
+    }
+
     public static byte[] decompress(byte[] data) throws Exception{
         if(data.length == 0)
             return null;

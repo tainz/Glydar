f44f1af40ac68aa9b2135d6fc234bd7b3f4d8148
Sean "CyberKitsune" McClenaghan <cyberkitsune09@gmail.com>
2013-08-03 20:32:53 -0700

    Better constructor until we have an API

--- src/main/java/org/glydar/glydar/netty/packet/shared/Packet10Chat.java
+++ src/main/java/org/glydar/glydar/netty/packet/shared/Packet10Chat.java
@@ -13,17 +13,25 @@ public class Packet10Chat extends CubeWorldPacket {
     byte[] messageBytes;
     String message;
 	Entity sender;
+    long senderID;
 
 	//DO NOT EVER TURN ON RECEIVING CACHING. THE SIGNATURES DIFFER!!!
 
-	public Packet10Chat() {
-
-	}
-
 	public Packet10Chat(String message, Entity sender) {
 		this.sender = sender;
+        this.senderID = sender.entityID;
 		this.message = message;
 	}
+
+    /**
+     * Creates a new chat packet instance, ready to be sent to clients.
+     * @param message Message to be sent
+     * @param senderID Entity ID of the sender or 0 if generic server message.
+     */
+    public Packet10Chat(String message, int senderID) {
+        this.message = message;
+        this.senderID = senderID;
+    }
 
     @Override
     protected void internalDecode(ByteBuf buf) {
@@ -36,8 +44,7 @@ public class Packet10Chat extends CubeWorldPacket {
 	@Override
 	protected void internalEncode(ByteBuf buf) {
 		byte[] msgBuf = message.getBytes(Charsets.UTF_16LE);
-		buf.writeLong(sender.entityID);
-		//TODO Force string encoding.
+		buf.writeLong(senderID);
 		buf.writeInt(msgBuf.length / 2);
 		buf.writeBytes(msgBuf);
 	}

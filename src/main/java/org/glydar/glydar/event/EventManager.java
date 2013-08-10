7e4bda63d4a58f08ccab711a9c38d470e6b71dc3
Daniel Shnell <theultimatebudgie@gmail.com>
2013-08-10 19:00:31 +1000

    Re-worked the event system. Now with priorities!

--- src/main/java/org/glydar/glydar/event/EventManager.java
+++ src/main/java/org/glydar/glydar/event/EventManager.java
@@ -9,105 +9,99 @@ import java.util.List;
 import org.glydar.glydar.Glydar;
 import org.glydar.glydar.plugin.Plugin;
 
-/**
- * @author The Glydar Team, YoshiGenius (For the base)
- */
-
 public class EventManager {
     
-    private EventManager(){}
-    // TODO: Replace String references to plugins with a Plugin object
-    private static HashMap<Listener, String> pluginListeners = new HashMap<Listener, String>();
-    private static HashMap<Class<? extends Event>, HashMap<Method, Listener>> eventMethods = new HashMap<Class<? extends Event>, HashMap<Method, Listener>>();
-    private static HashMap<Method, Boolean> ignoreCancelled = new HashMap<Method, Boolean>();
-    
-    public static HashMap<Method, Listener> getMethods(Class<? extends Event> evt) {
-        if (evt == null) {
-            return new HashMap<Method, Listener>();
-        }
-        if (eventMethods.get(evt) == null) {
-        	//Glydar.getServer().getLogger().info("Not found!");
-        	eventMethods.put(evt, new HashMap<Method, Listener>());
-            return eventMethods.get(evt);
-        }
-        //Glydar.getServer().getLogger().info("Found!");
-        return eventMethods.get(evt);
-    }
-    
-    public static String getPlugin(Listener listener) {
-        if (listener == null) {
-            return null;
-        }
-        if (pluginListeners.get(listener) == null) {
-            return null;
-        }
-        return pluginListeners.get(listener);
-    }
+    public EventManager(){}
+    private static List<StoredListener> storedListeners = new ArrayList<StoredListener>();
     
     public static boolean registerListener(Plugin plugin, Listener listener) {
-        if (plugin == null || listener == null || pluginListeners.containsKey(listener)) {
+        if (plugin == null || listener == null) {
             return false;
         }
+        StoredListener sl = new StoredListener(plugin, listener);
         for (Method m : listener.getClass().getMethods()) {
-            if (m.getParameterTypes().length == 0) {
+            if (m.getParameterTypes().length != 1) {
                 continue;
             }
             for (Annotation an : m.getAnnotations()) {
                 if (an.annotationType() == EventHandler.class) {
                     Class par1 = m.getParameterTypes()[0];
                     if (Event.class.isAssignableFrom(par1)) {
-                        Class<? extends Event> ec = (Class<? extends Event>) par1;
-                        HashMap<Method, Listener> methods = getMethods(ec);
-                        methods.put(m, listener);
-                        EventHandler eh = (EventHandler) an;
-                        ignoreCancelled.put(m, eh.ignoreCancelled());
-                        eventMethods.put(ec, methods);
-                        //Glydar.getServer().getLogger().info("Method: " + m.getName());
+                    	EventHandler eh = (EventHandler) an;
+                        sl.addMethod(par1.getName(), m, eh.priority(), eh.ignoreCancelled());
+                        Glydar.getServer().getLogger().info("Method: " + m.getName());
                     }
+                    break;
                 }
             }
         }
-        pluginListeners.put(listener, plugin.getName());
+        storedListeners.add(sl);
         return true;
+    }
+    
+    public static void unregisterListener(Listener listener){
+    	for (StoredListener sl : storedListeners){
+    		if (sl.getListener().equals(listener)){
+    			storedListeners.remove(sl);
+    		}
+    	}
+    }
+    
+    public static void unregisterListeners(Plugin plugin){
+    	for (StoredListener sl : storedListeners){
+    		if (sl.getPlugin().equals(plugin)){
+    			storedListeners.remove(sl);
+    		}
+    	}
     }
     
     @SuppressWarnings("CallToThreadDumpStack")
     public static Event callEvent(Event evt) {
+    	HashMap<Method, StoredListener> methods = new HashMap<Method, StoredListener>();
         if (evt == null) {
             return null;
         }
-        for (Class<? extends Event> ec : eventMethods.keySet()) {
-        	if (!evt.getClass().getName().equals(ec.getName())){
-        		continue;
+        
+        String evtName = evt.getClass().getName();
+        for (StoredListener sl : storedListeners){
+        	if (sl.hasEvent(evtName)){
+        		for (Method m : sl.getMethods(evtName)){
+        			methods.put(m, sl);
+        			//Glydar.getServer().getLogger().info("Method Found!");
+        		}
         	}
-            for (Method m : eventMethods.get(ec).keySet()) {
-                if (m == null) {
-                    continue;
-                }
-                if (evt instanceof Cancellable) {
-                    Cancellable cancellable = (Cancellable) evt;
-                    try {
-                        if (!(cancellable.isCancelled() && ignoreCancelled.get(m))) {
-                            m.invoke(eventMethods.get(ec).get(m), evt);
-                            //Glydar.getServer().getLogger().info("Method:" + m.getName());
+        }
+        
+        for (EventPriority ep : EventPriority.values()){
+        	for (Method m : methods.keySet()){
+        		if (methods.get(m).checkPriority(m, ep)){
+        			if (evt instanceof Cancellable) {
+                        Cancellable cancellable = (Cancellable) evt;
+                        try {
+                            if (!(cancellable.isCancelled() && methods.get(m).checkIgnoreCancelled(m))) {
+                                m.invoke(methods.get(m).getListener(), evt);
+                                //Glydar.getServer().getLogger().info("Method:" + m.getName());
+                            }
+                        } catch (Exception ex) {
+                        	//Glydar.getServer().getLogger().info("ERROR: Method:" + m.getName());
+                            ex.printStackTrace();
+                            methods.remove(m);
                         }
-                    } catch (Exception ex) {
-                    	//Glydar.getServer().getLogger().info("ERROR: Method:" + m.getName());
-                        ex.printStackTrace();
-                    }
-                } else {
-                    try {
-                       m.invoke(eventMethods.get(ec).get(m), evt);
-                       //Glydar.getServer().getLogger().info("Method:" + m.getName());
-                    } catch (Exception ex) {
-                    	//Glydar.getServer().getLogger().info("ERROR: Method:" + m.getName());
-                        ex.printStackTrace();
-                    }
-                }
-            }
+        			} else {
+	        			try{
+	    	        		m.invoke(methods.get(m).getListener(), evt);
+	    	        		//Glydar.getServer().getLogger().info(m.getName() + "Invoked!");
+	            		} catch (Exception e){
+	            			//Glydar.getServer().getLogger().info("ERROR: Method:" + m.getName());
+	            			e.printStackTrace();
+	            			methods.remove(m);
+	            		}
+        			}
+        		}
+        	}
         }
 
         return evt;
     }
-
+    
 }
